77ffe805257a96fe83ed118565106e674afbb828
Letícia Cotta <leticiacotta@gmail.com>
2022-04-04 20:28:47 -0300

    Tentando arrumar a desgraceira do login pt1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,64 @@
 import React from 'react';
-// import { Redirect } from 'react-router-dom';
-// import { createUser } from '../services/userAPI';
+import { userAction } from '../actions';
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      btnDisabler: true,
-      emailOk: true,
-      passwordOk: true,
-      inputEmail: '',
-      inputPassword: '',
-    }; // Fim do state
+// this.state = {
+//   btnDisabler: true,
+//   emailOk: true,
+//   passwordOk: true,
+//   inputEmail: '',
+//   inputPassword: '',
+// }; // Fim do state
 
-    function ultimateValidator({ target: value, name }) {
-      // Validador de emails, senhas, etc.
-      const minValue = 6;
-      const passValid = inputPassword.lenght >= minValue;
-      const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail);
-      const { passwordOk, emailOk } = this.state;
-
-      if (name === 'email') {
-        this.setState({ inputEmail: value });
-        if (value.match(ultimateValidator)) {
-          this.setState({ emailOk: true });
-        } else {
-          this.setState({ emailOk: false });
-        }
-      }
-    } // Fim da função
-  } // Fim do Constructor
-
-  render() {
-    return (
-      <div data-testid="page-login">
-        Login
-        <button type="button">Entrar</button>
-        {/* <Redirect to="/carteira" /> */}
-      </div>
-    );
-  }
+class Wallet extends React.Component {
+function ultimateValidator() {
+  // Validador de emails, senhas, etc.
+  const minValue = 6;
+  const passValid = inputPassword.lenght >= minValue;
+  const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail);
+  const inputs = [passValid, emailValid];
+  const validadeInputs = inputs.every((input) => input === true);
+  if (validadeInputs) func(false);
 }
 
-// - Realize as seguintes verificações nos campos de email e senha, de modo que caso sejam falsas o botão fique desabilitado:
-//   - O email está no formato válido, como 'alguem@alguem.com'.
-//   - A senha possui 6 ou mais caracteres.
-// - Salve o email no estado da aplicação, com a chave **_email_**, assim que a pessoa usuária logar.
-// - A rota deve ser mudada para '/carteira' após o clique no botão '**Entrar**'.
+function handleClick() {
+  // Função que muda rota para /carteira após clicar em ENTRAR [criar]
+  const { dispatch } = props;
+  dispatch(userAction(email));
+  history.push('/carteira');
+}
+//   } // Fim da função
+// } // Fim do Constructor
 
+// render() {
+return (
+  <div data-testid="page-login">
+    Email:
+    <input
+      type="email"
+      id="email"
+      data-testid="inputEmail"
+      name="email"
+      value={ email }
+      onChange={ (e) => setEmail(e.target.value) }
+    />
+
+    Senha:
+    <input
+      type="password"
+      id="password"
+      data-testid="inputPassword"
+      name="password"
+      value={ password }
+      onChange={ (e) => setPassword(e.target.value) }
+    />
+    <button
+      type="button"
+      disabled={ btnDisabler }
+      onClick={ () => handleClick() }
+    >
+      Entrar
+    </button>
+  </div>
+);
+}
 export default Login;
